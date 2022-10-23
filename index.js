ff27af71196840eafda669a29ff6ba08737dcfcc
rahulkallur <rahulkallur63@gmail.com>
2022-10-23 12:30:02 +0530

    added getDownloadURL API

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const AWS = require("aws-sdk");
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
 dotenv.config();
 const upload = multer({ storage: storage });
 
@@ -35,6 +48,29 @@ app.post("/api/file/upload", upload.single("file"), (req, res) => {
       location: data.Location,
     });
   });
+});
+
+app.post("/api/file/getDownloadURL", (req, res) => {
+  console.log("Req:",req.body)
+  let fileName = req.body.fileName;
+  let bucketName = req.body.bucketName;
+
+  const signedUrlExpireSeconds = 24 * 3600;
+
+  const url = s3Client.getSignedUrl("getObject", {
+    Bucket: bucketName,
+    Key: fileName,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  if (url) {
+    let obj = {
+      downloadURL: url,
+    };
+    res.status(200).send(obj);
+  } else {
+    res.status(409).send("File not found");
+  }
 });
 
 app.listen(process.env.SERVER_PORT, () => {
